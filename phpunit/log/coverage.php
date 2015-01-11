bb29da8b65859fcad4711f37782f631c4c91ff28
Stanislav WEB <stanisov@gmail.com>
2015-01-11 17:53:09 +0200

    PHP unit added tests

---
+++ phpunit/log/coverage.php
@@ -0,0 +1,404 @@
+<?php
+$coverage = new PHP_CodeCoverage;
+$coverage->setData(array (
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Validator.php' => 
+  array (
+    36 => 
+    array (
+    ),
+    37 => 
+    array (
+    ),
+    38 => 
+    array (
+    ),
+    42 => 
+    array (
+    ),
+    44 => 
+    array (
+    ),
+    45 => 
+    array (
+    ),
+    46 => NULL,
+    48 => 
+    array (
+    ),
+    49 => NULL,
+    62 => 
+    array (
+    ),
+    63 => 
+    array (
+    ),
+    64 => 
+    array (
+    ),
+    68 => 
+    array (
+    ),
+    70 => 
+    array (
+    ),
+    71 => 
+    array (
+    ),
+    72 => NULL,
+    74 => 
+    array (
+    ),
+    75 => NULL,
+    88 => 
+    array (
+    ),
+    89 => 
+    array (
+    ),
+    90 => 
+    array (
+    ),
+    94 => 
+    array (
+    ),
+    96 => 
+    array (
+    ),
+    98 => 
+    array (
+    ),
+    99 => NULL,
+    101 => 
+    array (
+    ),
+    102 => NULL,
+    115 => 
+    array (
+    ),
+    116 => 
+    array (
+    ),
+    117 => 
+    array (
+    ),
+    121 => 
+    array (
+    ),
+    123 => 
+    array (
+    ),
+    125 => 
+    array (
+    ),
+    126 => NULL,
+    128 => 
+    array (
+    ),
+    129 => NULL,
+    142 => 
+    array (
+    ),
+    143 => 
+    array (
+    ),
+    144 => 
+    array (
+    ),
+    148 => 
+    array (
+    ),
+    150 => 
+    array (
+    ),
+    151 => 
+    array (
+    ),
+    152 => NULL,
+    154 => 
+    array (
+    ),
+    156 => 
+    array (
+    ),
+    157 => 
+    array (
+    ),
+    158 => NULL,
+    160 => 
+    array (
+    ),
+    161 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Uploader.php' => 
+  array (
+    67 => 
+    array (
+      0 => 'Test\\Uploader\\UploaderTest::testConstructor',
+    ),
+    69 => 
+    array (
+    ),
+    71 => 
+    array (
+    ),
+    74 => 
+    array (
+      0 => 'Test\\Uploader\\UploaderTest::testConstructor',
+    ),
+    77 => 
+    array (
+      0 => 'Test\\Uploader\\UploaderTest::testConstructor',
+    ),
+    78 => 
+    array (
+      0 => 'Test\\Uploader\\UploaderTest::testConstructor',
+    ),
+    87 => 
+    array (
+    ),
+    88 => NULL,
+    99 => 
+    array (
+    ),
+    102 => 
+    array (
+    ),
+    103 => 
+    array (
+    ),
+    114 => 
+    array (
+    ),
+    116 => 
+    array (
+    ),
+    118 => 
+    array (
+    ),
+    119 => 
+    array (
+    ),
+    121 => 
+    array (
+    ),
+    123 => 
+    array (
+    ),
+    125 => 
+    array (
+    ),
+    126 => NULL,
+    136 => 
+    array (
+    ),
+    138 => 
+    array (
+    ),
+    142 => 
+    array (
+    ),
+    146 => 
+    array (
+    ),
+    148 => 
+    array (
+    ),
+    149 => 
+    array (
+    ),
+    150 => 
+    array (
+    ),
+    151 => 
+    array (
+    ),
+    152 => 
+    array (
+    ),
+    153 => 
+    array (
+    ),
+    155 => 
+    array (
+    ),
+    156 => NULL,
+    167 => 
+    array (
+    ),
+    169 => 
+    array (
+    ),
+    170 => 
+    array (
+    ),
+    171 => 
+    array (
+    ),
+    173 => 
+    array (
+    ),
+    174 => 
+    array (
+    ),
+    175 => 
+    array (
+    ),
+    176 => 
+    array (
+    ),
+    178 => 
+    array (
+    ),
+    179 => 
+    array (
+    ),
+    181 => 
+    array (
+    ),
+    182 => 
+    array (
+    ),
+    183 => 
+    array (
+    ),
+    185 => 
+    array (
+    ),
+    188 => 
+    array (
+    ),
+    190 => 
+    array (
+    ),
+    192 => 
+    array (
+    ),
+    193 => 
+    array (
+    ),
+    194 => 
+    array (
+    ),
+    195 => 
+    array (
+    ),
+    196 => 
+    array (
+    ),
+    197 => 
+    array (
+    ),
+    198 => 
+    array (
+    ),
+    200 => 
+    array (
+    ),
+    201 => NULL,
+    211 => 
+    array (
+    ),
+    213 => NULL,
+    222 => 
+    array (
+    ),
+    224 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Helpers/Format.php' => 
+  array (
+    47 => 
+    array (
+    ),
+    48 => 
+    array (
+    ),
+    49 => 
+    array (
+    ),
+    50 => NULL,
+    62 => 
+    array (
+    ),
+    64 => 
+    array (
+    ),
+    65 => 
+    array (
+    ),
+    67 => 
+    array (
+    ),
+    68 => 
+    array (
+    ),
+    69 => 
+    array (
+    ),
+    70 => 
+    array (
+    ),
+    71 => 
+    array (
+    ),
+    73 => 
+    array (
+    ),
+    75 => 
+    array (
+    ),
+    77 => 
+    array (
+    ),
+    78 => 
+    array (
+    ),
+    79 => 
+    array (
+    ),
+    81 => 
+    array (
+    ),
+    83 => 
+    array (
+    ),
+    84 => 
+    array (
+    ),
+    85 => 
+    array (
+    ),
+    87 => 
+    array (
+    ),
+    88 => NULL,
+  ),
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Helpers/Message.php' => 
+  array (
+    39 => 
+    array (
+    ),
+    40 => 
+    array (
+    ),
+    41 => NULL,
+    42 => 
+    array (
+    ),
+  ),
+));
+$coverage->setTests(array (
+  'Test\\Uploader\\UploaderTest::testConstructor' => 0,
+  'UNCOVERED_FILES_FROM_WHITELIST' => NULL,
+));
+
+$filter = $coverage->filter();
+$filter->setBlacklistedFiles(array (
+));
+$filter->setWhitelistedFiles(array (
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Helpers/Format.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Helpers/Message.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Uploader.php' => true,
+  '/var/www/phalcon-devtools/phalcon.local/vendor/stanislav-web/phalcon-uploader/src/Uploader/Validator.php' => true,
+));
+
+return $coverage;
